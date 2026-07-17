94aae47071dd1702f47efc322f3d3cd2da814bdd
74579919-sudo <74579919-sudo>
2026-07-17 18:45:30 -0500

    ProductoController Modificado

--- app/Http/Controllers/Api/ProductoController.php
+++ app/Http/Controllers/Api/ProductoController.php
@@ -46,7 +46,7 @@ public function index()
         $request->validate([
             'categoria_id' => 'required|integer|exists:categorias,id',
             'nombre' => 'required|string|max:100',
-            'precio' => 'required|numeric',
+            'precio_venta' => 'required|numeric',
             'stock' => 'required|integer',
         ]);
         $producto->update($request->all());
